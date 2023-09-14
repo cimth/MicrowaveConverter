27ce6f98eb3ea6fd698cbeb2d4982a7cbad38630
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2023-09-14 17:12:47 +0200

    Set a custom window size of 400 x 600

--- MicrowaveConverter/App.xaml.cs
+++ MicrowaveConverter/App.xaml.cs
@@ -2,10 +2,31 @@
 
 public partial class App : Application
 {
+    // ==============
+    // Initialization
+    // ==============
+
     public App()
     {
         InitializeComponent();
 
         MainPage = new AppShell();
+    }
+
+    // ==============
+    // Override CreateWindow()
+    // ==============
+
+    // Override the CreateWindow() method to set a custom window size.
+    // See: https://stackoverflow.com/questions/72399551/maui-net-set-window-size
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.Width = 400;
+        window.Height = 600;
+
+        return window;
     }
 }
