6c55f090c2969f9dc30e0372bfa7ccc87bfe32ad
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2023-09-17 20:44:17 +0200

    Fix name of EqualsToVisibilityConverter

--- MicrowaveConverter/Converters/EqualsToVisibilityConverter.cs
+++ MicrowaveConverter/Converters/EqualsToVisibilityConverter.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace MicrowaveConverter.Converters;
 
-public class EqualsConverter : IValueConverter
+public class EqualsToVisibilityConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
