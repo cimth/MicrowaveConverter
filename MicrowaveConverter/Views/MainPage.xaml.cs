077033634e8c28481c3ec389c380036f8d818ed7
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2023-09-20 16:36:12 +0200

    Hide the keyboard when unfocussing an Entry

--- MicrowaveConverter/Views/MainPage.xaml.cs
+++ MicrowaveConverter/Views/MainPage.xaml.cs
@@ -1,12 +1,29 @@
-﻿using MicrowaveConverter.ViewModels;
+﻿using CommunityToolkit.Maui.Core.Platform;
+using MicrowaveConverter.ViewModels;
 
 namespace MicrowaveConverter.Views;
 
 public partial class MainPage : ContentPage
 {
+    // ==============
+    // Initialization
+    // ==============
+
     public MainPage(MainViewModel mainViewModel)
     {
         InitializeComponent();
         BindingContext = mainViewModel;
+    }
+
+    // ==============
+    // Hide keyboard when unfocussing an Entry
+    // ==============
+
+    private void Entry_OnUnfocused(object? sender, FocusEventArgs e)
+    {
+        if (sender is Entry entry)
+        {
+            entry.HideKeyboardAsync(CancellationToken.None);
+        }
     }
 }
